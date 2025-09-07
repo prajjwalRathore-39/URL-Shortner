f88f8bef82676c1b5e00f288fa758d4ca3febb7a
prajjwalRathore-39 <prajjwalrathore39@gmail.com>
2025-09-07 22:41:29 +0530

    Modified URLShortnerApplication to check if the code is running.

--- src/main/java/com/url/url_shortner_sb/UrlShortnerSbApplication.java
+++ src/main/java/com/url/url_shortner_sb/UrlShortnerSbApplication.java
@@ -8,6 +8,7 @@ public class UrlShortnerSbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortnerSbApplication.class, args);
+        System.out.println("URL Shortner is Running!!");
 	}
 
 }
