3180971b65c75e230a53d19320dc058cdb0bd314
prajjwalRathore-39 <prajjwalrathore39@gmail.com>
2025-09-07 22:23:29 +0530

    Initial Commit

---
+++ src/test/java/com/url/url_shortner_sb/UrlShortnerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.url_shortner_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
